29a40ec05a1a2a0177c0260817267d702306d71d
Jay <panchalj272@gmail.com>
2022-10-04 14:43:27 +0530

    Features: Resolved Production Error

--- pages/auth/admin.js
+++ pages/auth/admin.js
@@ -1,15 +1,18 @@
 import Layout from "../../components/Layout";
 import { useSession } from "next-auth/react"
 import { useRouter } from 'next/router'
+import {useEffect} from "react"
 
 const AdminPage = () => {
     const  {data: session} = useSession();
     const router = useRouter()
 
-    if(!session) {
-        router.replace("/");
-        return null;
-    }
+    useEffect(() =>  {
+        if(!session) {
+            router.replace("/");
+            return null;
+        }
+    },[])
 
     return (
         <Layout showSubmitProjectBtn={false} showFooter={false}>
