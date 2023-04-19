fdb162a604cb0d1e90bf765fc8ea688a4fe2dbb6
Jay Panchal <66618767+JayP09@users.noreply.github.com>
2023-04-19 09:27:36 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = ({showFooter}) => {
            {showFooter ? (
                 <div className="py-5 px-4 sm:px-8 lg:px-12 xl:px-36">
                     <div className="text-center py-4 text-transparent bg-clip-text bg-gradient-to-r from-[#ff7c01] via-[#ff5301] to-[#a819eb] fontFamily">
-                        Copyright © 2022. Made with ♥ by <a target="blank" href="https://twitter.com/JayPTwts" className="cursor-pointer underline">@JayPTwts</a>
+                        Copyright © 2022. Made with ♥ by <a target="blank" href="https://twitter.com/s0rrymybad_" className="cursor-pointer underline">@s0rrymybad_</a>
                     </div>
                 </div>
             ):""}
